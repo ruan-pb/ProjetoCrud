273a0445d7e84b9eb4058c291a00aa445e2f132f
Ruan-_pb <ruancruzsoares@hotmail.com>
2020-10-02 14:54:42 -0300

    atualização

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,20 +1,28 @@
 package gui;
 
-import java.net.URL;
+import java.io.IOException;
 
+import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Service.DepartmentService;
 import model.entities.Department;
@@ -34,9 +42,12 @@ public class DepartmentListController implements Initializable {
 	
 	private ObservableList<Department> obsList;
 	
+	
+	//buscar a janela do formulario
 	@FXML
-	public void onButtonAction() {
-		System.out.println("Botao New em Ação");
+	public void onButtonAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartment(DepartmentService dpService) {
@@ -66,6 +77,27 @@ public class DepartmentListController implements Initializable {
 		List<Department>lista = departmentServico.findAll();
 		obsList = FXCollections.observableArrayList(lista);
 		tableViewDepartment.setItems(obsList);
+	}
+	private void createDialogForm(String absoluteName,Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			//carrega a janela do formulario para preencher
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com os dados do departmento");
+			
+			dialogStage.setScene(new Scene(pane));
+			
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.Aviso("IOException", "Erro ao carregar a pagina", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 
 }
